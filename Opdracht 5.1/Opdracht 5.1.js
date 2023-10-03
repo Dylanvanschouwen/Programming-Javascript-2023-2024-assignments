df1fbddfcefb6e8187fa97d972ce333e0d0e92d3
Dylanvanschouwen <9020515@student.zadkine.nl>
2023-10-03 09:31:30 +0200

    Opdracht 5.1 done

--- Opdracht 5.1/Opdracht 5.1.js
+++ Opdracht 5.1/Opdracht 5.1.js
@@ -7,5 +7,5 @@ function veranderKleur(kleur) {
 }
 
 function veranderTekst() {
-    document.getElementById("kop1").innerHTML = "Hello world!";
+    document.getElementById("kop1").innerHTML = "Een ID kan slechts één keer per pagina worden gebruikt omdat een ID uniek moet zijn binnen de HTML-pagina. Het wordt gebruikt om specifieke HTML-elementen te identificeren. Als je meerdere elementen met hetzelfde ID hebt, zou het niet meer uniek zijn, en dat kan leiden tot onverwacht gedrag in je code.";
 }
